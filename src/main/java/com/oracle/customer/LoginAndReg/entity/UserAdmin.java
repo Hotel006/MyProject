1c8f533d9462ef48a5226f74610646e7b962c6c5
mjh4251 <425106255@qq.com>
2019-12-16 11:57:39 +0800

    注册登录servlet1更新

---
+++ src/main/java/com/oracle/customer/LoginAndReg/entity/UserAdmin.java
@@ -0,0 +1,35 @@
+package com.oracle.customer.LoginAndReg.entity;
+
+public class UserAdmin {
+	private int uid;
+	private String uloginname;
+	private String uloginpass;
+	private String unikename;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUloginname() {
+		return uloginname;
+	}
+	public void setUloginname(String uloginname) {
+		this.uloginname = uloginname;
+	}
+	public String getUloginpass() {
+		return uloginpass;
+	}
+	public void setUloginpass(String uloginpass) {
+		this.uloginpass = uloginpass;
+	}
+	public String getUnikename() {
+		return unikename;
+	}
+	public void setUnikename(String unikename) {
+		this.unikename = unikename;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/oracle/customer/LoginAndReg/servlet/LoginServlet.java
@@ -0,0 +1,51 @@
+package com.oracle.customer.LoginAndReg.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oracle.customer.LoginAndReg.entity.UserAdmin;
+import com.oracle.customer.LoginAndReg.service.LoginAndRegService;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String phone = request.getParameter("loginname");
+		String pass = request.getParameter("loginpass");
+		LoginAndRegService ls = new LoginAndRegService();
+		
+		try {
+			UserAdmin ua = ls.login(phone, pass);
+			request.getSession().setAttribute("SESSIONUSER", ua);
+			request.getRequestDispatcher("").forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
