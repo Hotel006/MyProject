bedaba0b2200b2fbd93209d693fc39c0e86ae3db
leihaobin123 <2281353871@qq.com>
2019-12-16 22:19:27 +0800

    添加客户端订单dao层

--- src/main/java/com/oracle/customer/order/dao/CustomerOrderDao.java
+++ src/main/java/com/oracle/customer/order/dao/CustomerOrderDao.java
@@ -28,5 +28,24 @@ public class CustomerOrderDao {
 		return qer.query(sql, new BeanListHandler<Hotel_order>(Hotel_order.class));
 		
 	}
+	
+	public List<Hotel_order> queryByDetails() throws SQLException {
+		
+		QueryRunner qy = new QueryRunner(DBHelper.ds);
+		String sql = "SELECT\r\n" + 
+				"hotel_order.onumber,\r\n" + 
+				"hotel_order.oname,\r\n" + 
+				"hotel_order.ophone,\r\n" + 
+				"hotel_order.ohouse,\r\n" + 
+				"hotel_order.ocount,\r\n" + 
+				"hotel_order.omoney,\r\n" + 
+				"hotel_order.oontime,\r\n" + 
+				"hotel_order.oouttime,\r\n" + 
+				"hotel_order.ooderstate\r\n" + 
+				"FROM\r\n" + 
+				"hotel_order\r\n";
+		
+		return qy.query(sql, new BeanListHandler<Hotel_order>(Hotel_order.class));
+	}
 
 }
