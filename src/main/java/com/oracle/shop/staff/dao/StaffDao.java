71a5b08d5e1e51e4546cf5a35a1365020d56e42a
mjh4251 <425106255@qq.com>
2019-12-16 18:04:28 +0800

    添加员工和修改的逻辑层实现测试

--- src/main/java/com/oracle/shop/staff/dao/StaffDao.java
+++ src/main/java/com/oracle/shop/staff/dao/StaffDao.java
@@ -1,7 +1,28 @@
 package com.oracle.shop.staff.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+import com.oracle.entity.Hotel_staff;
 import com.oracle.util.DBHelper;
 
 public class StaffDao extends DBHelper {
-	
+	public List<Hotel_staff> queryAll() throws SQLException{
+		List<Hotel_staff> list = new ArrayList<Hotel_staff>();
+		
+		String sql = "select * from hotel_staff";
+		
+		
+		QueryRunner qr = new QueryRunner(DBHelper.ds);
+		list = qr.query(sql, new BeanListHandler<Hotel_staff>(Hotel_staff.class));
+		
+		
+
+		return list;
+		
+	}
 }
--- src/main/java/com/oracle/shop/staff/service/StaffService.java
+++ src/main/java/com/oracle/shop/staff/service/StaffService.java
@@ -1,5 +1,15 @@
 package com.oracle.shop.staff.service;
 
-public class StaffService {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.oracle.entity.Hotel_staff;
+import com.oracle.shop.staff.dao.StaffDao;
+
+public class StaffService {
+	public List<Hotel_staff> queryAll() throws SQLException {
+			StaffDao sd = new StaffDao();
+			return sd.queryAll();
+		
+	}
 }
