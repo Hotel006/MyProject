08fdf9f6cb0cdf1b44237d64a766b0b87b1a08cd
mjh4251 <425106255@qq.com>
2019-12-16 09:17:56 +0800

    登录注册建包和验证码的修改

---
+++ src/main/java/com/oracle/customer/LoginAndReg/dao/LogAndRegDao.java
@@ -0,0 +1,5 @@
+package com.oracle.customer.LoginAndReg.dao;
+
+public class LogAndRegDao {
+
+}
--- src/main/java/com/oracle/util/SMGutil.java
+++ src/main/java/com/oracle/util/SMGutil.java
@@ -6,38 +6,42 @@ import java.util.Map;
 import org.apache.http.HttpResponse;
 
 public class SMGutil {
-	public static void main(String[] args) {
-	    String host = "http://dingxin.market.alicloudapi.com";
-	    String path = "/dx/sendSms";
-	    String method = "POST";
-	    String appcode = "0360bec0484347ee95129f1b1228b05b";
-	    Map<String, String> headers = new HashMap<String, String>();
-	    //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
-	    headers.put("Authorization", "APPCODE " + appcode);
-	    Map<String, String> querys = new HashMap<String, String>();
-	    querys.put("mobile", "17691040476");
-	    querys.put("param", "code:1905");
-	    querys.put("tpl_id", "TP19120918");
-	    Map<String, String> bodys = new HashMap<String, String>();
+	
+	public void sendValidateCode(String phoneNO,String validateCode) {
+		
+		 String host = "http://dingxin.market.alicloudapi.com";
+		    String path = "/dx/sendSms";
+		    String method = "POST";
+		    String appcode = "0360bec0484347ee95129f1b1228b05b";
+		    Map<String, String> headers = new HashMap<String, String>();
+		    //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+		    headers.put("Authorization", "APPCODE " + appcode);
+		    Map<String, String> querys = new HashMap<String, String>();
+		    querys.put("mobile",phoneNO);
+		    querys.put("param", "code:"+validateCode+"");
+		    querys.put("tpl_id", "TP19120918");
+		    Map<String, String> bodys = new HashMap<String, String>();
 
 
-	    try {
-	    	/**
-	    	* 重要提示如下:
-	    	* HttpUtils请从
-	    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
-	    	* 下载
-	    	*
-	    	* 相应的依赖请参照
-	    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
-	    	*/
-	    	HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
-	    	System.out.println(response.toString());
-	    	//获取response的body
-	    	//System.out.println(EntityUtils.toString(response.getEntity()));
-	    } catch (Exception e) {
-	    	e.printStackTrace();
-	    }
+		    try {
+		    	/**
+		    	* 重要提示如下:
+		    	* HttpUtils请从
+		    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+		    	* 下载
+		    	*
+		    	* 相应的依赖请参照
+		    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+		    	*/
+		    	HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+		    	System.out.println(response.toString());
+		    	//获取response的body
+		    	//System.out.println(EntityUtils.toString(response.getEntity()));
+		    } catch (Exception e) {
+		    	e.printStackTrace();
+		    }
 	}
+	
+	   
 
 }
